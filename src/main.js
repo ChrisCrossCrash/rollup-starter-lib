e8855dbaae996472c2d907f739298c8259856173
Christopher Kumm <chrislkumm@protonmail.com>
2023-03-25 17:38:51 -0500

    Before trying to install npm package

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import foo from './foo.js'
-import { version } from '../package.json'
 
 export default function () {
-  console.log(version)
+  import('./foo.js').then(({ default: foo }) => console.log(foo))
 }
