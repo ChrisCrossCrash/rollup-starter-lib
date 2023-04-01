0253fb248fce14e35ac459db19bff4ea91ca7408
Christopher Kumm <chrislkumm@protonmail.com>
2023-04-01 12:52:55 -0500

    Install and configure `commonjs` plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
   input: 'src/main.js',
@@ -8,5 +9,13 @@ export default {
       format: 'cjs',
     },
   ],
-  plugins: [resolve()],
+  plugins: [
+    // The commonjs plugin converts CommonJS modules to ES modules
+    // so they are compatible with Rollup.
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    commonjs(),
+    // The node-resolve plugin tells Rollup how to find external modules.
+    // https://github.com/rollup/plugins/tree/master/packages/node-resolve
+    resolve(),
+  ],
 }
