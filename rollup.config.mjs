447f5296beb28404927986a2e04707c32804e198
Christopher Kumm <chrislkumm@protonmail.com>
2023-04-01 22:46:30 -0500

    Add terser plugin to config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
 
 export default {
   input: 'src/main.ts',
@@ -27,5 +28,7 @@ export default {
     // The node-resolve plugin tells Rollup how to find external modules.
     // https://github.com/rollup/plugins/tree/master/packages/node-resolve
     resolve(),
+    // The terser plugin minifies the output.
+    terser(),
   ],
 }
